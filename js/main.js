20c41447ee20710a30701cff41cb8e8c0d21fc1e
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2025-04-11 14:14:29 +0200

    add functionality to generate obstacles

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 
 class Player {
-    constructor(){
-        this.positionX = 50; // horizontal position (in vw)
-        this.positionY = 0; // vertical position (in vh)
+    constructor() {
         this.width = 10;
         this.height = 10;
+        this.positionX = 50; // horizontal position (in vw)
+        this.positionY = 0; // vertical position (in vh)
         this.updateUI();
     }
-    updateUI(){
+    updateUI() {
         const playerElm = document.getElementById("player");
-        playerElm.style.left = this.positionX + "vw";
-        playerElm.style.bottom = this.positionY + "vh";
         playerElm.style.width = this.width + "vw";
         playerElm.style.height = this.height + "vh";
+        playerElm.style.left = this.positionX + "vw";
+        playerElm.style.bottom = this.positionY + "vh";
     }
     moveLeft() {
         this.positionX--;
@@ -24,12 +24,66 @@ class Player {
     }
 }
 
-const player = new Player()
+
+class Obstacle {
+    constructor() {
+        this.width = 10;
+        this.height = 10;
+        this.positionX = Math.floor(Math.random() * (100 - this.width + 1)); // generate a random number between 0 and (100-width)
+        this.positionY = 100;
+        this.obstacleElm = null;
+
+        this.createDomElement();
+        this.updateUI();
+    }
+    createDomElement() {
+        // step1: create the element
+        this.obstacleElm = document.createElement("div");
+
+        // step2: add content or modify (eg. innerText, attributes...)
+        this.obstacleElm.className = "obstacle";
+
+        //step3: append to the dom: `parentElm.appendChild()`
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.obstacleElm);
+    }
+    updateUI() {
+        this.obstacleElm.style.width = this.width + "vw";
+        this.obstacleElm.style.height = this.height + "vh";
+        this.obstacleElm.style.left = this.positionX + "vw";
+        this.obstacleElm.style.bottom = this.positionY + "vh";
+    }
+    moveDown() {
+        this.positionY -= 0.5;
+        this.updateUI();
+    }
+}
+
+const player = new Player();
+const obstaclesArr = []; // will store instances of the class Obstacle
+
+
+// generate obstacles
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstaclesArr.push(newObstacle);
+}, 3000);
+
+
+// move obstacles
+setInterval(() => {
+    obstaclesArr.forEach((obstacleInstance) => {
+        obstacleInstance.moveDown();
+    })
+}, 16);
+
+
+
 
 document.addEventListener("keydown", (e) => {
-    if(e.code === 'ArrowLeft'){
+    if (e.code === 'ArrowLeft') {
         player.moveLeft();
-    } else if (e.code === 'ArrowRight'){
+    } else if (e.code === 'ArrowRight') {
         player.moveRight();
     }
 })
