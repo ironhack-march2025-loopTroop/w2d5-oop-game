059778ca9df376ad44735fef3848dc7ad5b92c4c
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2025-04-11 11:09:08 +0200

    add player functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-console.log("hello world");
+
+class Player {
+    constructor(){
+        this.positionX = 50; // horizontal position (in vw)
+        this.positionY = 0; // vertical position (in vh)
+        this.width = 10;
+        this.height = 10;
+        this.updateUI();
+    }
+    updateUI(){
+        const playerElm = document.getElementById("player");
+        playerElm.style.left = this.positionX + "vw";
+        playerElm.style.bottom = this.positionY + "vh";
+        playerElm.style.width = this.width + "vw";
+        playerElm.style.height = this.height + "vh";
+    }
+    moveLeft() {
+        this.positionX--;
+        this.updateUI();
+    }
+    moveRight() {
+        this.positionX++;
+        this.updateUI();
+    }
+}
+
+const player = new Player()
+
+document.addEventListener("keydown", (e) => {
+    if(e.code === 'ArrowLeft'){
+        player.moveLeft();
+    } else if (e.code === 'ArrowRight'){
+        player.moveRight();
+    }
+})
